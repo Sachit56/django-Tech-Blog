6370ff53b46372d1684b4cd9948b4a9a288a18bf
Sachit Khadka <sachitkhadka73@gmail.com>
2023-12-14 08:54:57 +0545

    Update

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,11 @@
 # example/urls.py
 from django.urls import path
 
-from example.views import index
+from example.views import index,UpdateView,DeleteView
 
 app_name='example'
 urlpatterns = [
     path('', index,name='home'),
+    path('update/<int:pk>',UpdateView,name='update'),
+    path('delete/<int:pk>',DeleteView,name='delete')
 ]
--- example/views.py
+++ example/views.py
@@ -1,15 +1,65 @@
-# example/views.py
-# from datetime import datetime
-
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import TaskForm
+from .models import TaskModel
 
 def index(request):
-    form=TaskForm()
+    tasks = TaskModel.objects.all()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+
+        try:
+            if form.is_valid():
+                form.save()
+                return redirect('/')
+        except Exception as e:
+            
+            print(f"Error saving form: {e}")
+
+    else:
+        form = TaskForm()
+
+    return render(request, 'example/index.html', {
+        'form': form,
+        'tasks': tasks
+    })
+
+def UpdateView(request, pk):
+    task = get_object_or_404(TaskModel, pk=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        try:
+            form = TaskForm(request.POST, instance=task)
+
+            if form.is_valid():
+                form.save()
+                return redirect('/')
+        except Exception as e:
+           
+            print(f"Error updating form: {e}")
+
+    else:
+        form = TaskForm(instance=task)
+
+    return render(request, 'example/update.html', {
+        'form': form
+    })
+
+def DeleteView(request,pk):
+    delete=get_object_or_404(TaskModel,pk=pk)
+
+    form=TaskForm(instance=delete)
+
+    if request.POST:
+        form=TaskForm(request.POST,instance=delete)
+
+        form.save()
+        return redirect('/')
     
-    return render(request,'example/index.html',{
+    return render(request,'example/delete.html',{
         'form':form
     })
-    
-    
+
+
+
